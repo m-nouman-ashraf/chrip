5b2e3ed7dcb03489900423801cf043829ed1905b
Nouman Ashraf <muhammad-nouman@hotmail.com>
2023-10-11 20:20:10 +0500

    Next Image added

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,41 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { User } from "@clerk/nextjs/dist/types/api";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-
+import { TRPCError } from "@trpc/server";
+const filterUserFromClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    imageUrl: user.imageUrl,
+  };
+};
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({ take: 100 });
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserFromClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for Post not found",
+        });
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
